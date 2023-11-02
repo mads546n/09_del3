d6aa5efd2f054506e0ebeb5e904ecb45dfdeb0a6
Dokey <asgerjd@gmail.com>
2023-11-02 23:31:29 +0100

    First commit of GameRunner

--- src/Main/GameRunner.java
+++ src/Main/GameRunner.java
@@ -2,6 +2,23 @@ package Main;
 
 public class GameRunner {
     public void main(String[] args){
+
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
+
+        var scanner = new java.util.Scanner(System.in);
+        scanner.useLocale(java.util.Locale.ENGLISH);
+        String input;
+
+        while (scanner.hasNextLine()){
+
+            input = scanner.nextLine();
+
+        }
+        scanner.close();
+
         
     }
     
