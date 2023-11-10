44190b44a518d0561fb023cb9a60ad1f818619ed
TobiasMelcher1309 <144644629+TobiasMelcher1309@users.noreply.github.com>
2023-11-10 11:17:43 +0100

    Defined icons, corrected language

--- src/Tile/TileManeger.java
+++ src/Tile/TileManeger.java
@@ -1,33 +1,54 @@
 package Tile;
 
 public class TileManeger {
-    public static Icon[] icons = {};
-    public static Tile[] tiles = new Tile[24];
-
-    static {
-        new NoActionTile(0, "Start", icon[0]);
-        new PropertyTile(1, "Brugerbar", icon[1], 1, 1);
-        new PropertyTile(2, "Pizzahus", icon[2], 1, 2);
-        new Chance(3, icon[3]);
-        new PropertyTile(4, "Slikbutikken", icon[4], 1, 1);
-        new PropertyTile(5, "Iskiosken", icon[5], 1, 1);
-        new JailTile(6, "Fængsel", icon[6]);
-        new PropertyTile(7, "Museet", icon[7], 2, 4);
-        new PropertyTile(8, "Biblioteket", icon[8], 2, 4);
-        new Chance(9, "Chance", icon[9]);
-        new PropertyTile(10, "Skatepark", icon[10], 2, 6);
-        new PropertyTile(11, "Swimmingpool", icon[11], 2, 6);
-        new NoActionTile(12, "Gratis parkering", icon[12]);
-        new PropertyTile(13, "Spillehallen", icon[13], 3, 8);
-        new PropertyTile(14, "Biografen", icon[14], 3, 8);
-        new Chance(15, icon[15]);
-        new PropertyTile(16, "Legetøjsbutikken", icon[16], 3, 10);
-        new PropertyTile(17, "Dyrehandlen", icon[17], 3, 10);
-        new JailTile(18, "Gå i fængsel", icon[18]);
-        new PropertyTile(19, "Bowlinghallen", icon[19], 4, 12);
-        new PropertyTile(20, "Zoologisk have", icon[20], 4, 12);
-        new Chance(21, "Chance", icon[21]);
-        new PropertyTile(22, "Vandlandet", icon[22], 4, 14);
-        new PropertyTile(23, "Strandpromenaden", icon[23], 5, 22);
-    }
+    public static Icon[] icons = {
+        new Icon("START", 2),
+        new Icon("BURGER", 1),
+        new Icon("PIZZA", 1),
+        new Icon("CHANCE", 0),
+        new Icon("CANDY.", 1),
+        new Icon("ICECREAM", 1),
+        new Icon("VISIT", 0),
+        new Icon("MUSEUM", 2),
+        new Icon("LIBRARY.", 2),
+        new Icon("CHANCE", 0),
+        new Icon("SKATE", 2),
+        new Icon("SWIMMING", 2),
+        new Icon("PARKING", 0),
+        new Icon("ARCADE", 3),
+        new Icon("CINEMA", 3),
+        new Icon("TOYSTORE", 3),
+        new Icon("PET SHOP", 3),
+        new Icon("JAIL", 0),
+        new Icon("BOWLING", 4),
+        new Icon("ZOO", 4),
+        new Icon("CHANCE", 0),
+        new Icon("WATERPK:", 5),
+        new Icon("BEACH", 5), 
+    };
+    public static Tile[] tiles = {
+        new NoActionTile(0, "Start", icons[0]),
+        new PropertyTile(1, "Burgerbar", icons[1], 1, 1),
+        new PropertyTile(2, "Pizzahus", icons[2], 1, 2),
+        new Chance(3, icons[3]),
+        new PropertyTile(4, "Candy Store", icons[4], 1, 1),
+        new PropertyTile(5, "Ice cream Store", icons[5], 1, 1),
+        new NoActionTile(6, "Visiting Jail", icons[6]),
+        new PropertyTile(7, "Museum", icons[7], 2, 4),
+        new PropertyTile(8, "Library", icons[8], 2, 4),
+        new Chance(9, icons[9]),
+        new PropertyTile(10, "Skatepark", icons[10], 2, 6),
+        new PropertyTile(11, "Swimmingpool", icons[11], 2, 6),
+        new NoActionTile(12, "Free parking", icons[12]),
+        new PropertyTile(13, "Arcase", icons[13], 3, 8),
+        new PropertyTile(14, "Cinema", icons[14], 3, 8),
+        new Chance(15, icons[15]),
+        new PropertyTile(16, "Toy Store", icons[16], 3, 10),
+        new PropertyTile(17, "Pet Shop", icons[17], 3, 10),
+        new JailTile(18, icons[18]),
+        new PropertyTile(19, "Bowlinghall", icons[19], 4, 12),
+        new PropertyTile(20, "Zoo", icons[20], 4, 12),
+        new Chance(2, icons[21]),
+        new PropertyTile(22, "Water Park", icons[22], 5, 14),
+        new PropertyTile(23, "The Beach Promenade", icons[23], 5, 22)};
 }
